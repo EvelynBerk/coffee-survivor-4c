c26608aa32155c77573afeb207b17a43891b2457
Laura Noiel <Laura.Noiel@hakju.at>
2025-12-02 12:42:57 +0100

    Score Manager

--- Assets/Scripts/TypingGame.cs
+++ Assets/Scripts/TypingGame.cs
@@ -1,20 +1,64 @@
 using UnityEngine;
 using TMPro;
 
-public class RandomLetter : MonoBehaviour
+public class TypingGame : MonoBehaviour
 {
-    public TextMeshProUGUI letterText;
-    private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static TypingGame Instance;
+    
 
+    [SerializeField] private TMP_Text letterText;
+    [SerializeField] private GameObject minigameUI;
+    
+    private char currentLetter;
+    private bool isActive = false;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+    
     void Start()
     {
-        DisplayRandomLetter();
+        GenerateNewLetter();
+        isActive = true;
     }
-
-    public void DisplayRandomLetter()
+    
+    public void Activate()
     {
+        isActive = true;
+        minigameUI.SetActive(true);
+        GenerateNewLetter();
+        Debug.Log("Minigame aktiviert");
+    }
+    
+    public void Deactivate()
+    {
+        isActive = false;
+        minigameUI.SetActive(false);
+        Debug.Log("Minigame deaktiviert");
+    }
+    
+    void GenerateNewLetter()
+    {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int randomIndex = Random.Range(0, alphabet.Length);
-        char randomLetter = alphabet[randomIndex];
-        letterText.text = randomLetter.ToString();
+        currentLetter = alphabet[randomIndex];
+        letterText.text = currentLetter.ToString();
+    }
+    
+    void Update()
+    {
+        if (!isActive) return;
+        
+        foreach (char c in Input.inputString)
+        {
+            if (char.ToUpper(c) == currentLetter)
+            {
+                Debug.Log("Richtig!");
+                ScoreManager.Instance.AddScore(10);
+                
+                GenerateNewLetter();
+            }
+        }
     }
 }
